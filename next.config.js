c05327750cdbca95cc510192b35831e6a5889383
Nathan Nguyen <67423224+nathan22x3@users.noreply.github.com>
2022-05-10 14:12:44 +0700

    chore(➖): remove `@svgr/webpack`

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ const withPWA = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
